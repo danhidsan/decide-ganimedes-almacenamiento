d28744fed822ef2f5ddf58927f23323127d237f7
joaquinperez94 <blogdeaula78@gmail.com>
2018-12-28 11:20:49 +0000

    feat: Añadida configuración para usar MongoDB

--- decide/decide-ganimedes-almacenamiento/bin/django-admin.py
+++ decide/decide-ganimedes-almacenamiento/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/joaquin/Documentos/Proyecto/decide-ganimedes-almacenamiento/decide/decide-ganimedes-almacenamiento/bin/python
+#!/home/joaquin/Documentos/Proyecto/decide-ganimedes-almacenamiento/decide/decide-ganimedes-almacenamiento/bin/python3.6
 from django.core import management
 
 if __name__ == "__main__":
--- decide/store/models.py
+++ decide/store/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from base.models import BigBigField
 
-
 class Vote(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
@@ -9,7 +8,14 @@ class Vote(models.Model):
     a = BigBigField()
     b = BigBigField()
 
-    voted = models.DateTimeField(auto_now=True)
+    voted = models.DateTimeField()
 
     def __str__(self):
         return '{}: {}'.format(self.voting_id, self.voter_id)
+
+# class Vote(Document):
+#     voting_id=fields.IntField()
+#     voter_id=fields.IntField()
+#     a=BigBigField()
+#     b=BigBigField()
+#     voted=fields.DateTimeField()
