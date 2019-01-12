eb1487e2c2d77b6bd9d63e2fb1b477eac72d6cc2
Daniel Hidalgo <dhidalgosanchez96@gmail.com>
2019-01-12 12:53:58 +0100

    build: eliminado docker_push.sh
        - Se ha decidido construir la imagen de docker a través de GitHub

--- docker/docker_push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_HUB_PASS" | docker login -u "$DOCKER_HUB_USER" --password-stdin
-echo "Building Decide image"
-docker build -t decide_web:$TRAVIS_JOB_ID ./docker
-docker tag decide_web danhidsan/decide_web
-echo "Upload changes to Docker Hub"
-docker push danhidsan/decide_web
-echo "Image updated in Docker Hub"
