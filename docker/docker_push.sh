b10df7b7d48e75bf79dd7fc0711b3aa3a6b01438
Daniel Hidalgo <dhidalgosanchez96@gmail.com>
2019-01-12 11:55:19 +0100

    chore: Cambios para volver a activar la subida a Docker Hub
        - Se ha vuelto a añadir el fichero docker_push.sh para subir nuestro imagen de Decide a Docker Hub
        - Se ha vuelto a activar el comando compilemessages para comprobar su funcionamiento

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "$DOCKER_HUB_PASS" | docker login -u "$DOCKER_HUB_USER" --password-stdin
+echo "Building Decide image"
 docker build ./docker
 docker tag decide_web danhidsan/decide_web
+echo "Upload changes to Docker Hub"
 docker push danhidsan/decide_web
+echo "Image updated in Docker Hub"
